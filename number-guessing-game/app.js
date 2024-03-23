c74bf6612a3b8b405d3c44d130be63d33f00fadd
rehanmirza76 <abdulrehan0317@gmail.com>
2024-03-23 18:57:20 +0500

    new commit

---
+++ number-guessing-game/app.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "guessedNumber",
+        type: "number",
+        message: "Guess a Number:",
+    },
+]);
+if (answers.guessedNumber === randomNumber) {
+    console.log("You Guessed the correct Number :)");
+}
+else {
+    console.log("You Guessed the wrong number!!!");
+}
---
+++ simple-calc/main.ts
@@ -0,0 +1,40 @@
+#! /usr/bin/env  node 
+
+
+import inquirer from "inquirer";
+
+
+const answer = await inquirer.prompt([
+  { message: "Enter First Number", type: "number", name: "firstNumber" },
+  { message: "Enter Second Number", type: "number", name: "secondNumber" },
+  {
+    message: "Select an Operator",
+    type: "list",
+    name: "operator",
+    choices: ["Add", "Subtract", "Multiply", "Divide"],
+  },
+]);
+ 
+if ((answer.operator === "Add")) {
+  console.log(
+    `\n Your Answer is: ${answer.firstNumber + answer.secondNumber}` 
+  );
+}
+else if  ((answer.operator === "Subtract")) {
+  console.log(
+    `\n Your Answer is: ${answer.firstNumber - answer.secondNumber}` 
+  );
+}
+else if ((answer.operator === "Multiply")) {
+  console.log(
+    `\n Your Answer is: ${answer.firstNumber * answer.secondNumber}` 
+  );
+}
+else if ((answer.operator === "Divide")) {
+  console.log(
+    `\n Your Answer is: ${answer.firstNumber/answer.secondNumber}` 
+  );
+}
+else{
+  console.log("Please Select a Valid Operator!")
+}
