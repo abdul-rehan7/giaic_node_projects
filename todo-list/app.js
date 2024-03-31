356d7730ea810151fd69f080be16d4d1d9511b22
rehanmirza76 <abdulrehan0317@gmail.com>
2024-03-31 11:40:24 +0500

    new commit

--- todo-list/app.js
+++ todo-list/app.js
@@ -1,5 +1,6 @@
 #! /usr/bin/env node 
 import inquirer from "inquirer";
+import chalk from "chalk";
 let todos = [];
 let condition = true;
 //////////////// ADDING TODO'S ////////////////
@@ -8,16 +9,19 @@ while (condition) {
         {
             name: "todo",
             type: "input",
-            message: "\n What do you want to add to your todo's : ",
+            message: chalk.black.white("\t What do you want to add to your todo's : "),
         },
         {
             name: "addmore",
             type: "confirm",
             default: "false",
-            message: "\n Do you want to add more todo's? ",
+            message: chalk.cyan("\n Do you want to add more todo's? "),
         },
     ]);
     todos.push(addtodo.todo);
     condition = addtodo.addmore;
-    console.log(todos);
+    console.log(chalk.blueBright.underline("\n Your ToDo List :"));
+    todos.forEach(todo => {
+        console.log(chalk.green(`\t \u2022 ${todo}`));
+    });
 }
--- todo-list/app.ts
+++ todo-list/app.ts
@@ -2,6 +2,8 @@
 
 
 import inquirer from "inquirer";
+import chalk from "chalk"
+import { todo } from "node:test";
 
 let todos = [];
 let condition = true;
@@ -13,17 +15,24 @@ while (condition) {
     {
       name: "todo",
       type: "input",
-      message: "\n What do you want to add to your todo's : ",
+      message: chalk.black.white("\t What do you want to add to your todo's : "),
     },
 
     {
       name: "addmore",
       type: "confirm",
       default: "false",
-      message: "\n Do you want to add more todo's? ",
+      message: chalk.cyan("\n Do you want to add more todo's? "),
     },
   ]);
+ 
   todos.push(addtodo.todo)
   condition = addtodo.addmore;
-  console.log(todos);
+
+  console.log(chalk.blueBright.underline("\n Your ToDo List :"))
+  todos.forEach(todo => {
+    console.log(chalk.green(`\t \u2022 ${todo}`));
+  });
+    
+    
 }
