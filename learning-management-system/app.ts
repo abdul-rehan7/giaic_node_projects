c615a085ec574539d4ebb0991770a19a43a00110
rehanmirza76 <abdulrehan0317@gmail.com>
2024-04-30 10:02:40 +0500

    commit

--- learning-management-system/app.ts
+++ learning-management-system/app.ts
@@ -1,44 +1,52 @@
-import inquirer from "inquirer"
+import inquirer from "inquirer";
 
-let info = await inquirer.prompt (
-    [
-        {
-            name:"studentName",
-            type:"input",
-            message:"Enter the name of Student: "
-        },
+// ------------------------TAKING STUDENT INFO--------------------------
+let info = await inquirer.prompt([
+  {
+    name: "studentName",
+    type: "input",
+    message: "Enter the name of Student: ",
+  },
 
-        {
-            name:"courses",
-            type:"input",
-            message:"Enter Course Which you want to enroll: "
-        }
+  {
+    name: "courses",
+    type: "input",
+    message: "Enter Course Which you want to enroll: ",
+  },
+]);
 
-    ]
-)
-
-
-
+// ------------------------STUDENT DATA VARIABLE--------------------------
 let student = {
-    name:[info.studentName] ,
-    rollNumber: Math.floor(10000 + Math.random() * 10000) ,
-    courses:[info.courses],
-    Balance:1000,
-    feeStatus:"UNPAID"
+  name: [info.studentName],
+  rollNumber: Math.floor(10000 + Math.random() * 10000),
+  courses: [info.courses],
+  Balance: 100000,
+  feeStatus: 0,
+};
+
+// ------------------------PRINTING DATA--------------------------
+console.log(`\t Name : ${student.name}`);
+console.log(`\t Roll Number : ${student.rollNumber}`);
+console.log(`\t Courses : ${student.courses}`);
+console.log(`\t Balance : ${student.Balance}`);
+console.log(`\t Status : ${student.feeStatus}`);
+
+// ------------------------FEE PAYMENT--------------------------
+if (student.feeStatus != 100000) {
+  console.log("\t Your Fee is UNPAID!");
+
+  let pay = await inquirer.prompt({
+    name: "paying",
+    type: "number",
+    message: "Enter the Amount to Pay :",
+  });
+
+  console.log(`User Paid Rs ${pay.paying}`);
+  student.feeStatus += pay.paying;
+  student.Balance -= pay.paying;
+console.log(`Your Current Balance is : ${student.Balance}`)
 }
 
-
-
-console.log(`\t Name : ${student.name}`)
-console.log(`\t Roll Number : ${student.rollNumber}`)
-console.log(`\t Courses : ${student.courses}`)
-console.log(`\t Balance : ${student.Balance}`)
-console.log(`\t Status : ${student.feeStatus}`)
-
-if (student.feeStatus != 'PAID'){
-
-console.log("\t Your Fee is UNPAID!")
-
+else if (student.feeStatus = 100000){
+    console.log("\t Your Fee is PAID and You can Join the Couses")
 }
-
-
