a7eccfc3a83351b71cc5cd193cfe1c35a7e6faf3
rehanmirza76 <abdulrehan0317@gmail.com>
2024-05-04 23:02:05 +0500

    commit

--- adventure-game/app.js
+++ adventure-game/app.js
@@ -19,25 +19,25 @@ while (player.health > 0) {
     });
     //  ======================================= OPPONENTS =======================================
     if (ask.Q1 == "Skeleton") {
-        console.log("\nYou Selected to Fight with Skeleton");
+        console.log("\n You Selected to Fight with Skeleton");
         player.health -= 20;
-        console.log(chalk.yellow("\t AWWWW! You got a Damage of -20"));
+        console.log(chalk.yellow("\t Oh No! You got a Damage of -20"));
         player.health = Math.max(0, player.health);
-        console.log(`\nYour player health is ${chalk.greenBright(player.health)}`);
+        console.log(`\n Your Health: ${chalk.greenBright(player.health)} \n Enemy Health: ${chalk.greenBright(enemy.health)}`);
     }
     else if (ask.Q1 == "Alien") {
-        console.log("\nYou Selected to Fight with Alien");
+        console.log("\n You Selected to Fight with Alien");
         player.health -= 50;
-        console.log(chalk.yellow("\t AWWWW! You got a Damage of -50"));
+        console.log(chalk.yellow("\t Oh No! You got a Damage of -50"));
         player.health = Math.max(0, player.health);
-        console.log(`\nYour player health is ${chalk.greenBright(player.health)}`);
+        console.log(`\n Your Health: ${chalk.greenBright(player.health)} \n Enemy Health: ${chalk.greenBright(enemy.health)}`);
     }
     else if (ask.Q1 == "Robot") {
-        console.log("\nYou Selected to Fight with Robot");
+        console.log("\n You Selected to Fight with Robot");
         player.health -= 70;
-        console.log(chalk.yellow("\t AWWWW! You got a Damage of -70"));
+        console.log(chalk.yellow("\t Oh No! You got a Damage of -70"));
         player.health = Math.max(0, player.health);
-        console.log(`\nYour player health is ${chalk.greenBright(player.health)}`);
+        console.log(`\n Your Health: ${chalk.greenBright(player.health)} \n Enemy Health: ${chalk.greenBright(enemy.health)}`);
     }
     let secQ = await inquirer.prompt({
         name: "Q2",
@@ -47,17 +47,30 @@ while (player.health > 0) {
     });
     // ===================================== RECOVER ====================================
     {
-        if (secQ.Q2 = "Recover") {
+        if (secQ.Q2 == "Recover") {
             recover();
-            console.log("\nYou Choosed to Recover! ");
-            console.log(`Your Health is Now ${chalk.greenBright(player.health)}`);
+            console.log("\n You Choosed to Recover! ");
+            console.log(` Your Health: ${chalk.greenBright(player.health)} \n Enemy Health: ${chalk.greenBright(enemy.health)}`);
         }
-        else if (secQ.Q2 = "Attack") {
-            console.log("\nYou Choosed to Attack! ");
-            enemy.health -= 50;
-            console.log(`Your Enemy's Health is Now ${chalk.greenBright(enemy.health)}`);
+        // ===================================== ATTACK ====================================
+        else if (secQ.Q2 == "Attack") {
+            console.log("\n You Choosed to Attack! ");
+            enemy.health -= 40;
+            enemy.health = Math.max(0, enemy.health);
+            console.log(` Your Health: ${chalk.greenBright(player.health)} \n Enemy Health: ${chalk.greenBright(enemy.health)}`);
+            while (enemy.health <= 0) {
+                console.log(chalk.rgb(198, 91, 207)("Your Enemy Die and You won the Game :) "));
+                process.exit();
+            }
+        }
+        else {
+            console.log("Wrong Command!");
         }
     }
+}
+while (player.health <= 0) {
+    console.log("Your Health is 0 and You Loose the Game!");
+    process.exit();
 }
 // ===================================== GAME OVER ====================================
 console.log(chalk.red("\n \t GAME OVER !!!"));
