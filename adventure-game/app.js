b3eef8741788bbaeb5ed4815abda31f47e231135
rehanmirza76 <abdulrehan0317@gmail.com>
2024-05-04 20:44:25 +0500

    commit

--- adventure-game/app.js
+++ adventure-game/app.js
@@ -1,1 +1,30 @@
-export {};
+import inquirer from "inquirer";
+let player = {
+    health: 100,
+};
+// =================================== WHILE CONDITION ====================================
+while (player.health > 0) {
+    let ask = await inquirer.prompt({
+        name: "Q1",
+        message: "Which opponent Do You want to Fight?",
+        type: "list",
+        choices: ["Skeleton", "Alien", "Robot"]
+    });
+    //  ======================================= OPPONENTS =======================================
+    if (ask.Q1 == "Skeleton") {
+        console.log("You Selected to Fight with Skeleton");
+        console.log(`Your player health is ${player.health = player.health - 20}`);
+    }
+    else if (ask.Q1 == "Alien") {
+        console.log("You Selected to Fight with Alien");
+        console.log(`Your player health is ${player.health = player.health - 50}`);
+    }
+    else if (ask.Q1 == "Robot") {
+        console.log("You Selected to Fight with Robot");
+        console.log(`Your player health is ${player.health = player.health - 70}`);
+    }
+    else {
+        console.log("No Opponent Selected");
+    }
+}
+console.log("GAME OVER !!!");
--- adventure-game/app.ts
+++ adventure-game/app.ts
@@ -1,3 +1,44 @@
-import inquirer from "inquirer"
-import chalk from "chalk"
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+
+let player = {
+  health: 100,
+};
+
+// =================================== WHILE CONDITION ====================================
+ while(player.health > 0 ){
+ let ask = await inquirer.prompt(
+    {
+        name:"Q1",
+        message: "Which opponent Do You want to Fight?",
+        type:"list",
+        choices:["Skeleton","Alien","Robot"]
+    }
+ )
+
+
+//  ======================================= OPPONENTS =======================================
+
+ if(ask.Q1 == "Skeleton"){
+    console.log("You Selected to Fight with Skeleton")
+    console.log(`Your player health is ${player.health= player.health-20}`)
+ }
+else if (ask.Q1 == "Alien"){
+    console.log("You Selected to Fight with Alien")
+    console.log(`Your player health is ${player.health= player.health-50}`)
+ }
+ else if (ask.Q1 == "Robot"){
+    console.log("You Selected to Fight with Robot")
+    console.log(`Your player health is ${player.health= player.health-70}`)
+ }
+ else{
+    console.log("No Opponent Selected")
+ }
+
+}
+
+console.log(chalk.red("\n \t GAME OVER !!!"));
+
+
 
