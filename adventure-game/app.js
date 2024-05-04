d49329329d2ca920998a1139f86efb8cedede17c
rehanmirza76 <abdulrehan0317@gmail.com>
2024-05-04 21:02:30 +0500

    commit

--- adventure-game/app.js
+++ adventure-game/app.js
@@ -1,4 +1,5 @@
 import inquirer from "inquirer";
+import chalk from "chalk";
 let player = {
     health: 100,
 };
@@ -13,18 +14,27 @@ while (player.health > 0) {
     //  ======================================= OPPONENTS =======================================
     if (ask.Q1 == "Skeleton") {
         console.log("You Selected to Fight with Skeleton");
-        console.log(`Your player health is ${player.health = player.health - 20}`);
+        player.health -= 20;
+        console.log(chalk.red("\t AWWWW! You got a Damage of -20"));
+        player.health = Math.max(0, player.health);
+        console.log(`Your player health is ${chalk.greenBright(player.health)}`);
     }
     else if (ask.Q1 == "Alien") {
         console.log("You Selected to Fight with Alien");
-        console.log(`Your player health is ${player.health = player.health - 50}`);
+        player.health -= 50;
+        console.log(chalk.red("\t AWWWW! You got a Damage of -50"));
+        player.health = Math.max(0, player.health);
+        console.log(`Your player health is ${chalk.greenBright(player.health)}`);
     }
     else if (ask.Q1 == "Robot") {
         console.log("You Selected to Fight with Robot");
-        console.log(`Your player health is ${player.health = player.health - 70}`);
+        player.health -= 70;
+        console.log(chalk.red("\t AWWWW! You got a Damage of -70"));
+        player.health = Math.max(0, player.health);
+        console.log(`Your player health is ${chalk.greenBright(player.health)}`);
     }
     else {
         console.log("No Opponent Selected");
     }
 }
-console.log("GAME OVER !!!");
+console.log(chalk.red("\n \t GAME OVER !!!"));
