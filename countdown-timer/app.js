5f0938ff78df2e9189e5913c88b5295129dbdd63
rehanmirza76 <abdulrehan0317@gmail.com>
2024-05-12 21:13:24 +0500

    Working on it...

--- countdown-timer/app.js
+++ countdown-timer/app.js
@@ -1,11 +1,12 @@
-import inquirer from "inquirer";
-const ans = await inquirer.prompt({
-    name: "startingTime",
-    type: "input",
-    message: "Enter Your Starting time (in seconds)",
-});
+// const ans = await inquirer.prompt({
+//   name: "startingTime",
+//   type: "input",
+//   message: "Enter Your Starting time (in seconds)",
+// });
 function startTime(val) {
     setInterval((() => { }), 1000);
 }
 const inTime = new Date().setSeconds(new Date().getSeconds() + 1);
-console.log(inTime);
+const intervalTime = new Date(inTime);
+console.log(intervalTime);
+export {};
